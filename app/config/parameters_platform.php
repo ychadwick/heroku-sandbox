56640e19c6b53291f1b83b5141045d5794f36858
Yorkie Chadwick <ychadwick@gmail.com>
2015-11-19 11:44:02 +0100

    update DB config in parameters php file

--- app/config/parameters_platform.php
+++ app/config/parameters_platform.php
@@ -5,15 +5,15 @@ if (!$databaseUrl) {
 }
 
 
-$dbopts = parse_url(getenv('CLEARDB_DATABASE_URL'));
+$dbopts = parse_url($databaseUrl);
 
 
 $container->setParameter('database_driver', 'pdo_' . $dbopts['scheme']);
 $container->setParameter('database_host', $dbopts['host']);
 //$container->setParameter('database_port', $dbopts['port']);
 $container->setParameter('database_name', str_replace('/', '', $dbopts['path']));
-$container->setParameter('database_user', $dbopts['username']);
-$container->setParameter('database_password', $dbopts['password']);
+$container->setParameter('database_user', $dbopts['user']);
+$container->setParameter('database_password', $dbopts['pass']);
 $container->setParameter('database_path', '');
 
 # Hack.
--- db_test.php
+++ db_test.php
@@ -1,3 +1,19 @@
 <?php
+//$databaseUrl = "mysql://bb3ec6884ad029:d8ac9323@us-cdbr-iron-east-03.cleardb.net/heroku_fc56980a3ecdb8a?reconnect=true";
 $databaseUrl = getenv("CLEARDB_DATABASE_URL");
-var_dump($databaseUrl);
+
+$dbopts = parse_url($databaseUrl);
+
+var_dump($dbopts);
+
+$dbSettings = array();
+
+$dbSettings['database_driver'] = 'pdo_' . $dbopts['scheme'];
+$dbSettings['database_host'] = $dbopts['host'];
+//$dbSettings['database_port'] = $dbopts['port'];
+$dbSettings['database_name'] = str_replace('/', '', $dbopts['path']);
+$dbSettings['database_user'] = $dbopts['user'];
+$dbSettings['database_password'] = $dbopts['pass'];
+$dbSettings['database_path'] = '';
+
+var_dump($dbSettings);
